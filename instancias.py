70fd93f4c6a70c7403ed5722f79066a176e72f7a
Valery Maragaño <valery88_7@hotmail.com>
2024-08-22 20:10:11 -0400

    Se modifica instancia.

--- instancias.py
+++ instancias.py
@@ -1,14 +1,18 @@
 from te import Te
 
+# Crear dos instancias
 te_1 = Te()
-te_2 = "Te"
+te_2 = Te()
 
+# Almacenar el tipo de dato
 tipo_1 = type(te_1)
 tipo_2 = type(te_2)
 
+# Mostrar el tipo de dato
 print(tipo_1)
 print(tipo_2)
 
+# Comparar si ambos tipos son iguales
 if tipo_1 == tipo_2:
     print("Ambos objetos son del mismo tipo")
 else:
